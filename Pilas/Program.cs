dd52a93f58ca009495c8b3364ae5e462c0dc4637
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-26 23:56:40 -0700

    Pilas

--- Pilas/Program.cs
+++ Pilas/Program.cs
@@ -6,7 +6,62 @@ namespace Pilas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Pilas();
+        }
+
+        static void Pilas()
+        {
+            try
+            {
+                ArrayPilas pila = new ArrayPilas(5);
+
+                pila.Agregar("Cero");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Uno");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Dos");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Tres");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Cuatro");
+                Console.WriteLine(pila.Imprimir());
+
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
